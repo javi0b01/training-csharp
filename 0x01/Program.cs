c74dba30e6eb562d0d8733785f1f6c107f4d6635
javi <55107141+javi0b01@users.noreply.github.com>
2025-09-01 21:18:42 -0500

    training

--- 0x01/Program.cs
+++ 0x01/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿// comment inline
+/* comment
+multi
+line
+*/
+
+/* Console.Write("print with 'Console.Write'");
+Console.WriteLine("print with 'Console.WriteLine'"); */
+
+// Data types
+// Integer types
+byte my_byte = 255;
+Console.WriteLine(my_byte);
+sbyte my_sbyte = -128;
+Console.WriteLine(my_sbyte);
+short my_short = 32767;
+Console.WriteLine(my_short);
+ushort my_ushort = 65535;
+Console.WriteLine(my_ushort);
+int my_int = -2147483648;
+Console.WriteLine(my_int);
+uint my_uint = 4294967295;
+Console.WriteLine(my_uint);
+long my_long = -9223372036854775808L;
+Console.WriteLine(my_long);
+ulong my_ulong = 18446744073709551615UL;
+Console.WriteLine(my_ulong);
+// Floating point types
+float my_float = 3.14159F;
+Console.WriteLine(my_float);
+double my_double = 3.141592653589793;
+Console.WriteLine(my_double);
+decimal my_decimal = 99.99M;
+Console.WriteLine(my_decimal);
+// Booleans
+bool my_bool = true;
+Console.WriteLine(my_bool);
+// Characters
+char my_char = 'A';
+Console.WriteLine(my_char);
+// Strings
+string my_string = "Hello, World!";
+Console.WriteLine(my_string);
