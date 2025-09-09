49c12ed9fefa78c28b3e10d42e943abc8b8fc5b0
javi <55107141+javi0b01@users.noreply.github.com>
2025-09-08 21:00:09 -0500

    training

--- 0x01/Program.cs
+++ 0x01/Program.cs
@@ -9,35 +9,79 @@ Console.WriteLine("print with 'Console.WriteLine'"); */
 
 // Data types
 // Integer types
-byte my_byte = 255;
-Console.WriteLine(my_byte);
-sbyte my_sbyte = -128;
-Console.WriteLine(my_sbyte);
-short my_short = 32767;
-Console.WriteLine(my_short);
-ushort my_ushort = 65535;
-Console.WriteLine(my_ushort);
-int my_int = -2147483648;
-Console.WriteLine(my_int);
-uint my_uint = 4294967295;
-Console.WriteLine(my_uint);
-long my_long = -9223372036854775808L;
-Console.WriteLine(my_long);
-ulong my_ulong = 18446744073709551615UL;
-Console.WriteLine(my_ulong);
+byte myByte = 255;
+Console.WriteLine(myByte);
+sbyte mySbyte = -128;
+Console.WriteLine(mySbyte);
+short myShort = 32767;
+Console.WriteLine(myShort);
+ushort myUshort = 65535;
+Console.WriteLine(myUshort);
+int myInt = -2147483648;
+Console.WriteLine(myInt);
+uint myUint = 4294967295;
+Console.WriteLine(myUint);
+long myLong = -9223372036854775808L;
+Console.WriteLine(myLong);
+ulong myUlong = 18446744073709551615UL;
+Console.WriteLine(myUlong);
 // Floating point types
-float my_float = 3.14159F;
-Console.WriteLine(my_float);
-double my_double = 3.141592653589793;
-Console.WriteLine(my_double);
-decimal my_decimal = 99.99M;
-Console.WriteLine(my_decimal);
+float myFloat = 3.14159F;
+Console.WriteLine(myFloat);
+double myDouble = 3.141592653589793;
+Console.WriteLine(myDouble);
+decimal myDecimal = 99.99M;
+Console.WriteLine(myDecimal);
 // Booleans
-bool my_bool = true;
-Console.WriteLine(my_bool);
+bool myBool = true;
+Console.WriteLine(myBool);
 // Characters
-char my_char = 'A';
-Console.WriteLine(my_char);
+char myChar = 'A';
+Console.WriteLine(myChar);
 // Strings
-string my_string = "Hello, World!";
-Console.WriteLine(my_string);
+string myString = "Hello, World!";
+Console.WriteLine(myString);
+
+// Variables
+// Explicit
+string myExplicitVariable = "works!";
+Console.WriteLine(myExplicitVariable);
+
+// Implicit, local and fix type
+var myImplicitVariable = "works!";
+Console.WriteLine(myImplicitVariable);
+
+// Constant
+const string myConstant = "works!";
+Console.WriteLine(myConstant);
+
+// Scope
+string myGlobal = "I'm global";
+Console.WriteLine(myGlobal);
+{
+  string myLocal = "I'm local";
+  Console.WriteLine(myLocal);
+  var myLocalVar = "I'm local with var";
+  Console.WriteLine(myLocalVar);
+  Console.WriteLine(myGlobal);
+}
+
+// Strings
+string str1 = "Hello";
+string str2 = "World";
+// Concatenation
+string strResult = str1 + str2;
+Console.WriteLine(strResult);
+Console.WriteLine(str1 + ", " + str2 + "!");
+// Interpolation
+strResult = $"{str1}, {str2}!!";
+Console.WriteLine(strResult);
+Console.WriteLine("{0}, Mundo!", str1);
+Console.WriteLine("{0}, {1}.", str1, str2);
+
+// Casting
+// Implicit
+int int1 = 10;
+double double1 = int1;
+Console.WriteLine(double1);
+// Explicit
